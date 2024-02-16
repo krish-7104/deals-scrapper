940eec5880c3abfa5d8dc0ce9024a9ad64a205e0
Krish Jotaniya <krishjotaniya71@gmail.com>
2024-02-16 23:11:42 +0530

    Backend: myntra deal scrapper formatting

--- backend/deals-scrapper/myntra.js
+++ backend/deals-scrapper/myntra.js
@@ -1,57 +1,46 @@
 const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
 const cheerio = require('cheerio');
 const fs = require("fs");
-const StealthPlugin = require('puppeteer-extra-plugin-stealth')
 
 puppeteer.use(StealthPlugin())
 
-const getDealsData = async (url) => {
+const getAmazonDealsScrapper = async () => {
     console.log("\Myntra Deals Scrap Started")
-    const browser = await puppeteer.launch({ headless: true });
-    const page = await browser.newPage();
-    await page.goto(url, { waitUntil: 'domcontentloaded' });
-    const htmlContent = await page.content();
-    const $ = cheerio.load(htmlContent);
-    const deals = $(".product-base").toArray();
-    const allData = await Promise.all(deals.map(async (element) => {
-        const product = $(element);
-        return {
-            title: product.find(".product-product").text().trim() + " (" + product.find(".product-brand").text().trim() + ")",
-            image: product.find("img").first().attr("src"),
-            original_price: parseInt(product.find(".product-strike").text().replace("Rs. ", "").replace(/[^\d.]/g, '')),
-            discount_price: parseInt(product.find(".product-discountedPrice").first().text().replace("Rs. ", "").replace(/[^\d.]/g, '')),
-            discount: parseInt(product.find(".product-discountPercentage").last().text().replace(/[^\d.]/g, '')),
-            link: "https://www.myntra.com/" + product.find("a").first().attr("href")
-        };
-    }));
-    await browser.close();
-    fs.appendFileSync("log.txt", `Myntra Deals Scrapper Run at ${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}\n`);
-    console.log("Myntra Deals Scrap Ended")
-    return allData;
-};
-
-const getMyntraDealsScrapper = async () => {
+    let allData = [];
     try {
-        let totalPages = 10;
-        let allData = [];
-        let promises = [];
-
-        for (let i = 1; i <= totalPages; i++) {
-            promises.push(getDealsData(`https://www.myntra.com/deals?p=${i}`));
+        for (let i = 0; i < 5; i++) {
+            const browser = await puppeteer.launch({ headless: true });
+            const page = await browser.newPage();
+            await page.goto(`https://www.myntra.com/deals?p=${i}`);
+            await page.waitForSelector("img");
+            const htmlContent = await page.content();
+            const $ = cheerio.load(htmlContent);
+            const products = $(".product-base")
+            const productsData = await Promise.all(products.map(async (index, element) => {
+                const product = $(element);
+                return {
+                    title: product.find(".product-product").text().trim() + " (" + product.find(".product-brand").text().trim() + ")",
+                    image: product.find("img").attr("src"),
+                    original_price: parseInt(product.find(".product-strike").text().replace("Rs. ", "").replace(/[^\d.]/g, '')),
+                    discount_price: parseInt(product.find(".product-discountedPrice").first().text().replace("Rs. ", "").replace(/[^\d.]/g, '')),
+                    discount: parseInt(product.find(".product-discountPercentage").last().text().replace(/[^\d.]/g, '')),
+                    link: "https://www.myntra.com/" + product.find("a").first().attr("href")
+                };
+            }));
+            allData.push(productsData)
+            await browser.close();
         }
 
-        const dealsDataArrays = await Promise.all(promises);
-
-        dealsDataArrays.forEach(dealsData => {
-            allData.push(...dealsData);
-        });
-
-        fs.writeFileSync("myntra.json", JSON.stringify(allData, null, 2));
-        return allData;
+        const data = allData.reduce((acc, currentData) => {
+            return acc.concat(currentData);
+        }, []);
+        fs.writeFileSync("myntra.json", JSON.stringify(data, null, 2));
+        fs.appendFileSync("log.txt", `Myntra Deals Scrapper Run at ${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}\n`);
+        console.log("Myntra Deals Scrap Ended")
     } catch (error) {
-        console.error("Error while scraping deals:", error);
-        throw error;
+        console.error('Error scraping data:', error);
     }
-};
+}
 
-module.exports = getMyntraDealsScrapper
+module.exports = getAmazonDealsScrapper
