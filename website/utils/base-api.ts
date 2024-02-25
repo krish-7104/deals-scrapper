b38fab0fbab918089e440513e2dad5c56f243c98
Krish Jotaniya <krishjotaniya71@gmail.com>
2024-02-25 13:26:25 +0530

    Frontend: Updated api (next public)

--- website/utils/base-api.ts
+++ website/utils/base-api.ts
@@ -1,3 +1,3 @@
 // export const API_LINK = "http://192.168.29.135:4000/api/v1"; //network
 // export const API_LINK = "http://localhost:4000/api/v1";
-export const API_LINK = process.env.REACT_APP_BACKENDLINK;
+export const API_LINK = process.env.NEXT_PUBLIC_BACKENDLINK;
